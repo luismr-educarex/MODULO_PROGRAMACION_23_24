2aa7f723a96c6548922da623e41bc989ad927437
luismr-educarex <luismr@educarex.es>
2023-09-28 11:44:59 +0200

    Primer commit

---
+++ src/unidad1/Programa.java
@@ -0,0 +1,22 @@
+package unidad1;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+	    System.out.println("Esto es un saludo desdel mi primer programa");
+	    
+	    String ciclo = "Desarrollo de Aplicaciones Web";
+	    
+	    String modulo = "Programación";
+	    
+	    String unidad = "Unidad 1: Introducción a la programación";
+
+	    String punto = "Colaboradores en GitHub";
+	    
+	    String apartado = "Añadir Colaborador";
+	    
+	    String apartado2 = "Asignar permisos a colaborador";
+	}
+
+}
