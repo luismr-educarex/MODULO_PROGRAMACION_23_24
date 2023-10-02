d2e2228173b9829a4a994895c4d31187020f01fb
luismr-educarex <luismr@educarex.es>
2023-10-02 12:42:16 +0200

    Se borran los datos

--- src/unidad1/Programa.java
+++ src/unidad1/Programa.java
@@ -6,17 +6,6 @@ public class Programa {
 		
 	    System.out.println("Esto es un saludo desdel mi primer programa");
 	    
-	    String ciclo = "Desarrollo de Aplicaciones Web";
-	    
-	    String modulo = "Programación";
-	    
-	    String unidad = "Unidad 1: Introducción a la programación";
-
-	    String punto = "Colaboradores en GitHub";
-	    
-	    String apartado = "Añadir Colaborador";
-	    
-	    String apartado2 = "Asignar permisos a colaborador";
 	}
 
 }
