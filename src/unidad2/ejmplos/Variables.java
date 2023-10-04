79b9eea69a59d00acc52875146e817a2bd6b728b
ciclofp <ciclofp@10.0.2.15>
2023-10-04 10:23:44 +0200

    ejemplo variables

---
+++ src/unidad2/ejmplos/Variables.java
@@ -0,0 +1,40 @@
+package unidad2.ejmplos;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+		//nombre de variables y tipos de datos
+		//DATOS DEL PROPIETARIO
+		String dni="67887678H"; //almacena el número+letra
+		String nombrePropietario;
+		String direccion;
+		String telefono; //+34876-348-944
+		String cp;
+		//DATOS DEL ANIMAL
+		long  id;
+		String nombreAnimal;
+		char sexo;
+		int edad;
+		String raza;
+		String enfermedad;
+		boolean estaVacunado;
+		float peso;
+		boolean estaCastrado;
+		
+		edad=20;	
+		dni="67887678H";
+		
+		System.out.println("Edad:"+edad);
+		System.out.println("Dni:"+dni);
+		
+		edad=45;
+		System.out.println("Edad:"+edad);
+		
+		
+		
+		
+		
+	}
+
+}
