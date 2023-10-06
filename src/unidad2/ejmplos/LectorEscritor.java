eb7b5fa2ac780d5bca91e7892a93b48d640c94ad
ciclofp <ciclofp@10.0.2.15>
2023-10-06 09:34:01 +0200

    Codigo jueves

--- src/unidad2/ejmplos/LectorEscritor.java
+++ src/unidad2/ejmplos/LectorEscritor.java
@@ -8,13 +8,22 @@ public class LectorEscritor {
 		
 		int dato;
 		int dato2;
-		System.out.print("Dame un número entero:");
+		double dato3;
+		String nombre;
 		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Introzca su nombre:");
+		//nombre = entrada.nextLine();
+		nombre = entrada.next();
+		
+		
+		System.out.print("Dame un número entero:");
+		
 	
 		dato = entrada.nextInt();	
-		System.out.println("Usted ha introducido el número:"+dato);
+		System.out.println(nombre+" ha introducido el número:"+dato);
 		
-		System.out.print("Dame otro:");	
+		System.out.print("Dame otro,"+nombre+":");	
 		dato2 = entrada.nextInt();
 		System.out.println("Y ahora:"+dato2);
 		
@@ -23,6 +32,13 @@ public class LectorEscritor {
 		System.out.println("La suma es:"+suma);
 		System.out.println("La suma es:"+dato+dato2);
 		System.out.println("La suma es:"+(dato+dato2));
+		
+		System.out.print("Dame su saldo en el banco:");
+		 dato3 = entrada.nextDouble();
+		 
+		 System.out.print("Su saldo es:"+dato3);
+		 
+		System.out.print("Muchas gracias por todo "+nombre);
 		
 		
 		entrada.close();
---
+++ src/unidad2/ejmplos/Literales.java
@@ -0,0 +1,24 @@
+package unidad2.ejmplos;
+
+public class Literales {
+
+	public static void main(String[] args) {
+		
+		int b=0b00000101_00111001_01111111_10110001;
+		
+		System.out.println(b);
+		
+		int binario=0b0000_0101_0011_1001_0111_1111_1011_0001;
+		int hexadecimal=0x5___3___9___7___F___B___1;
+		int octal=01001;
+		int binario2=0b01001;
+		
+		System.out.println(binario);
+		System.out.println(hexadecimal);
+		System.out.println(octal);
+		System.out.println(binario2);
+		
+		
+	}
+
+}
