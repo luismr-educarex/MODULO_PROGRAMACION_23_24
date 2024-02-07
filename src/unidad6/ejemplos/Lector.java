fc95758634e618e2de74cb18d0c6efb145b595da
profesor <profesor@equipo-00>
2024-02-07 08:33:56 +0100

    Inicio de la clase Lector

---
+++ src/unidad6/ejemplos/Lector.java
@@ -0,0 +1,13 @@
+package unidad6.ejemplos;
+
+import java.util.Scanner;
+
+public class Lector {
+	
+	public static Scanner lector = new Scanner(System.in);
+
+	// No está terminado
+	public static int leerEntero() {
+		return 0;
+	}
+}
