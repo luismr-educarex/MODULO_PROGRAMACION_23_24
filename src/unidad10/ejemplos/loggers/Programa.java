0510c7486410b4a1da5a995bcac426a1372dc7a3
profesor <profesor@equipo-00>
2024-03-22 09:03:12 +0100

    EJemplo y ficheros log4j

---
+++ src/unidad10/ejemplos/loggers/Programa.java
@@ -0,0 +1,17 @@
+package unidad10.ejemplos.loggers;
+
+import org.apache.log4j.Logger;
+
+public class Programa {
+
+	static Logger logger = Logger.getLogger(Programa.class);
+	
+	public static void main(String[] args) {
+		
+
+		
+		
+
+	}
+
+}
