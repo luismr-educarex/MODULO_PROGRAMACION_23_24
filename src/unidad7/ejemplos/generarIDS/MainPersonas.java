c6baf5be3811e425578dc1a0a031858b237df14f
luismr-educarex <luismr@educarex.es>
2024-04-12 23:01:38 +0200

    Ejemplo crear objetos con id basado en un contador

---
+++ src/unidad7/ejemplos/generarIDS/MainPersonas.java
@@ -0,0 +1,17 @@
+package unidad7.ejemplos.generarIDS;
+
+public class MainPersonas {
+
+	public static void main(String[] args) {
+		
+
+		Persona p1 = new Persona("Michael");
+		Persona p2 = new Persona("Lebron");
+		Persona p3= new Persona("Koby");
+		
+		System.out.println(p1.toString());
+		System.out.println(p2.toString());
+		System.out.println(p3.toString());
+	}
+
+}
