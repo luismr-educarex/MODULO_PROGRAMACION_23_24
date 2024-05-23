8c619cc41507c3632f514458346fa59bb6abac02
Luis Martinez <lmartinezr10@educarex.es>
2024-05-23 14:21:58 +0200

    Código para empezar el ejercicio de las paneles y plantas solares

---
+++ src/practicasfinales/panelessolares/PanelSolar.java
@@ -0,0 +1,67 @@
+package practicasfinales.panelessolares;
+
+public class PanelSolar {
+	
+	/**
+	 * Constructor al que se le pasa el identificador del panel
+	 */
+	public PanelSolar(String id) {
+		
+	}
+	
+	/**
+	 * Retorna el identificador del panel
+	 */
+	public String obtenerId() {
+		
+	}
+	
+	/**
+	 * Método para alinear el panel; 
+	 * retorna true si se ha conseguido alinear con el sol, y false en caso contrario
+	 */
+	
+	public boolean panelAlineado() {
+		
+	}
+	
+	/**
+	 * Retorna el acimut del panel, en grados, que nos indica hacia dónde apunta en la dirección 
+	 * horizontal. El Sur se representa con el valor 180 grados. 
+	 * Lanza Averiado si el panel está averiado.
+	 * */
+	public double acimut() throws Averiado{
+		
+	}
+	
+	/**
+	 * Retorna la elevación del panel, en grados, sobre el horizonte. 
+	 * Cuando el panel apunta al horizonte el valor es 0 grados.
+	 * Lanza averiado si el panel está averiado
+	 */
+	
+	public double elevacion() throws Averiado{
+		
+	}
+	
+	/**
+	 * Retorna la potencia eléctrica, en vatios, que el panel está produciendo en este instante
+	 */
+	public double potencia() {
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/practicasfinales/panelessolares/PlantaSolar.java
@@ -0,0 +1,35 @@
+package practicasfinales.panelessolares;
+
+public class PlantaSolar {
+	
+	/**
+	 * Añade un  nuevo panel solar, que se pasa como parámetro.
+	 * Si ya existe uno con el mismo identificador lanza YaExiste.
+	 * En otro caso, añadel el panel en la lista de paneles
+	 */
+	
+	public void anadePanel(PanelSolar p) throws YaExiste{
+		
+	}
+	
+	/**
+	 * Alinea todos los paneles de la planta y con los que resulten correctamente alineados 
+	 * y no estén averiados calcula el acimut y elevacion medios, almaceándolos en los respectivos
+	 * atributos
+	 */
+	
+	public void alinea() {
+		
+	}
+	
+	/**
+	 * Encuentra y retorna un papel anómalo, que es aquel que sin estar averiado,
+	 * su acimut y elevación se diferencia en más de un 5% de los valores medios almacenados
+	 * en los atributos.
+	 * Retorna null si no hay paneles que cumplan esa condición
+	 */
+	 public PanelSolar panelAnomalo() {
+		 
+	 }
+
+}
